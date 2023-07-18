34dd9cc87d38c8740f5e75a014cdb88557647b75
kulsumluna <kulsumislamdmpi@gmail.com>
2023-07-18 20:13:42 +0600

    gfg

--- DSA/Module 23/insert_into_a_binary_search_tree.cpp
+++ DSA/Module 23/insert_into_a_binary_search_tree.cpp
@@ -1,8 +1,34 @@
-#include<bits/stdc++.h>
+// #include <bits/stdc++.h> 
+// /************************************************************
 
-using namespace std;
-int main()
-{
-    cout<<"abc";
-    return 0;
-}
+//     Following is the TreeNode class structure
+
+//     template <typename T>
+//     class TreeNode {
+//        public:
+//         T val;
+//         TreeNode<T> *left;
+//         TreeNode<T> *right;
+
+//         TreeNode(T val) {
+//             this->val = val;
+//             left = NULL;
+//             right = NULL;
+//         }
+//     };
+
+// ************************************************************/
+
+// TreeNode<int>* insertionInBST(TreeNode<int>* root, int val)
+// {
+//     if(root == NULL){
+//         root = new TreeNode<int>(val);
+//         return root;
+//     }
+//     if(root->val > val){
+//         root->left = insertionInBST(root->left,val);
+//     }else{
+//         root->right = insertionInBST(root->right,val);
+//     }
+//     return root;
+// }
